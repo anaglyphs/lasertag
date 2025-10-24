9ca8d50891ad570c7f56bd83016f8f1f1e31e90d
Julian Triveri <triveri@outlook.com>
2025-10-24 17:04:51 -0400

    server menu work

--- Assets/Anaglyph/LaserTag/Operator/ServerPanel.cs
+++ Assets/Anaglyph/LaserTag/Operator/ServerPanel.cs
@@ -12,20 +12,27 @@ namespace Anaglyph.Lasertag.Operator
 {
 	public sealed class ServerWindow : EditorWindow
 	{
+		[MenuItem("Lasertag/Server Menu")]
+		private static void ShowWindow()
+		{
+			var window = GetWindow<ServerWindow>("Server Menu");
+			window.minSize = new Vector2(320, 200);
+		}
+		
 		private const string TagSizeSaveKey = "Lasertag.tagSize";
 		private float tagSizeCm = 10f;
-		
+
 		private const string UseRelaySaveKey = "Lasertag.useRelay";
-		private bool useRelay;
-		
+		private bool useRelay = false;
+
 		private const string RoomNameSaveKey = "Lasertag.roomName";
 		private string roomName = "";
-		
+
 		private const string IpSaveKey = "Lasertag.ip";
 		private string ipAddress = "127.0.0.1";
-		
+
 		private MatchSettings settings = MatchSettings.DemoGame();
-		
+
 		private PageParentElement networkPages;
 		private PageParentElement matchPages;
 
@@ -35,19 +42,12 @@ namespace Anaglyph.Lasertag.Operator
 
 		private VisualElement matchSettingsPage;
 		private VisualElement matchRunningPage;
-	
-		[MenuItem("Lasertag/Server Menu")]
-		private static void ShowWindow()
-		{
-			var window = GetWindow<ServerWindow>("Server Menu");
-			window.minSize = new Vector2(320, 200);
-		}
 
 		private void OnEnable()
 		{
 			LoadPrefs();
 			CreateUI();
-			
+
 			NetcodeManagement.StateChange += UpdateHostingPage;
 			MatchReferee.StateChanged += UpdateMatchPage;
 		}
@@ -60,24 +60,24 @@ namespace Anaglyph.Lasertag.Operator
 
 		private void LoadPrefs()
 		{
-			tagSizeCm = EditorPrefs.GetFloat(TagSizeSaveKey, 10f);
-			useRelay = EditorPrefs.GetBool(UseRelaySaveKey, false);
-			roomName = EditorPrefs.GetString(RoomNameSaveKey, "");
-			ipAddress = EditorPrefs.GetString(IpSaveKey, "127.0.0.1");
+			tagSizeCm = EditorPrefs.GetFloat(TagSizeSaveKey, tagSizeCm);
+			useRelay = EditorPrefs.GetBool(UseRelaySaveKey, useRelay);
+			roomName = EditorPrefs.GetString(RoomNameSaveKey, roomName);
+			ipAddress = EditorPrefs.GetString(IpSaveKey, ipAddress);
 		}
-		
-		private void UpdateHostingPage(NetcodeManagement.NetworkState state) 
+
+		private void UpdateHostingPage(NetcodeManagement.NetworkState state)
 		{
 			switch (state)
 			{
 				case NetcodeManagement.NetworkState.Disconnected:
 					networkPages.SetActiveElement(startServerPage);
 					break;
-				
+
 				case NetcodeManagement.NetworkState.Connecting:
 					networkPages.SetActiveElement(connectingPage);
 					break;
-				
+
 				case NetcodeManagement.NetworkState.Connected:
 					networkPages.SetActiveElement(connectedPage);
 					break;
@@ -97,7 +97,7 @@ namespace Anaglyph.Lasertag.Operator
 			}
 			else
 			{
-				if(state == MatchState.NotPlaying)
+				if (state == MatchState.NotPlaying)
 					matchPages.SetActiveElement(matchSettingsPage);
 				else
 					matchPages.SetActiveElement(matchRunningPage);
@@ -109,7 +109,7 @@ namespace Anaglyph.Lasertag.Operator
 			ColocationManager.Instance.HostAprilTagSize = tagSizeCm;
 			ColocationManager.Instance.HostColocationMethod = ColocationManager.Method.AprilTag;
 			MainPlayer.Instance.SetIsParticipating(false);
-			
+
 			if (useRelay)
 			{
 				NetcodeManagement.ConnectUnityServices(roomName);
@@ -119,7 +119,7 @@ namespace Anaglyph.Lasertag.Operator
 				var manager = NetworkManager.Singleton;
 				var transport = manager.GetComponent<UnityTransport>();
 				transport.SetConnectionData(ipAddress, NetcodeManagement.port);
-				
+
 				NetcodeManagement.Host(NetcodeManagement.Protocol.LAN);
 			}
 		}
@@ -136,200 +136,206 @@ namespace Anaglyph.Lasertag.Operator
 			rootVisualElement.style.paddingLeft = 6;
 			rootVisualElement.style.paddingRight = 6;
 			rootVisualElement.style.paddingTop = 6;
-			
+
 			networkPages = new PageParentElement();
+			{
+				startServerPage = new VisualElement();
+				{
+					startServerPage.Add(new Label("Host Settings")
+						{ style = { unityFontStyleAndWeight = FontStyle.Bold } });
+
+					var tagField = new FloatField("AprilTag size (cm)") { value = tagSizeCm };
+					tagField.RegisterValueChangedCallback(evt =>
+					{
+						tagSizeCm = Mathf.Max(0f, evt.newValue);
+						EditorPrefs.SetFloat(TagSizeSaveKey, tagSizeCm);
+					});
+					startServerPage.Add(tagField);
+					
+					
+					var useRelayField = new Toggle("Use Relay") { value = useRelay };
+					useRelayField.RegisterValueChangedCallback(evt =>
+					{
+						useRelay = evt.newValue;
+						EditorPrefs.SetBool(UseRelaySaveKey, useRelay);
+					});
+					startServerPage.Add(useRelayField);
+					
+					
+					var protocolPages = new PageParentElement();
+					{
+						var lanPage = new VisualElement();
+						protocolPages.Add(lanPage);
+						{
+							var ipField = new TextField("IP") { value = ipAddress };
+							ipField.RegisterValueChangedCallback(evt =>
+							{
+								ipAddress = evt.newValue;
+								EditorPrefs.SetString(IpSaveKey, ipAddress);
+							});
+							lanPage.Add(ipField);
+						}
+
+						var relayPage = new VisualElement();
+						protocolPages.Add(relayPage);
+						{
+
+							var roomNameField = new TextField("Room Name") { value = roomName };
+							roomNameField.RegisterValueChangedCallback(evt =>
+							{
+								roomName = Regex.Replace(evt.newValue, @"[^a-zA-Z0-9]", "");
+								roomNameField.SetValueWithoutNotify(roomName);
+								EditorPrefs.SetString(RoomNameSaveKey, roomName);
+							});
+							relayPage.Add(roomNameField);
+						}
+						
+						// update visibility
+						protocolPages.SetActiveElement(useRelay ? relayPage : lanPage);
+						useRelayField.RegisterValueChangedCallback(evt =>
+						{
+							protocolPages.SetActiveElement(useRelay ? relayPage : lanPage);
+						});
+					}
+					startServerPage.Add(protocolPages);
+
+
+					var hostButton = new Button(() =>
+					{
+						if (EditorApplication.isPlaying)
+							StartHost();
+						else
+							EditorApplication.EnterPlaymode();
+					})
+					{
+						text = "Host",
+						style = { height = 32 }
+					};
+					startServerPage.Add(hostButton);
+
+					startServerPage.Add(
+						new Label(
+							"If using a Windows hotspot, remember to give Unity an exception in Windows Firewall.")
+						{
+							style =
+							{
+								whiteSpace = WhiteSpace.Normal,
+							}
+						});
+				}
+				networkPages.Add(startServerPage);
+
+
+				connectingPage = new VisualElement();
+				{
+					connectingPage.Add(new Label("Connecting...")
+						{ style = { unityFontStyleAndWeight = FontStyle.Bold } });
+				}
+				networkPages.Add(connectingPage);
+
+
+				connectedPage = new VisualElement();
+				{
+					connectedPage.Add(new Label("Hosting") { style = { unityFontStyleAndWeight = FontStyle.Bold } });
+
+					var stopButton = new Button(NetcodeManagement.Disconnect)
+					{
+						text = "Stop Hosting",
+						style = { height = 24 }
+					};
+					connectedPage.Add(stopButton);
+
+
+					// match pages
+					matchPages = new PageParentElement();
+					{
+						// match settings menu
+						matchSettingsPage = new VisualElement();
+						{
+
+							var matchSettingsLabel = new Label("Match Settings")
+								{ style = { unityFontStyleAndWeight = FontStyle.Bold, marginTop = 10 } };
+							matchSettingsPage.Add(matchSettingsLabel);
+
+							var respawnToggle = new Toggle("Respawn In Bases") { value = settings.respawnInBases };
+							respawnToggle.RegisterValueChangedCallback(evt => settings.respawnInBases = evt.newValue);
+							matchSettingsPage.Add(respawnToggle);
+
+							var respawnTime = new FloatField("Respawn Seconds") { value = settings.respawnSeconds };
+							respawnTime.RegisterValueChangedCallback(evt =>
+								settings.respawnSeconds = Mathf.Max(0f, evt.newValue));
+							matchSettingsPage.Add(respawnTime);
+
+							var regen = new FloatField("Health Regen / s") { value = settings.healthRegenPerSecond };
+							regen.RegisterValueChangedCallback(evt =>
+								settings.healthRegenPerSecond = Mathf.Max(0f, evt.newValue));
+							matchSettingsPage.Add(regen);
+
+							var ppk = new IntegerField("Points Per Kill") { value = settings.pointsPerKill };
+							ppk.RegisterValueChangedCallback(evt =>
+								settings.pointsPerKill = (byte)Mathf.Clamp(evt.newValue, 0, 255));
+							matchSettingsPage.Add(ppk);
+
+							var pps = new IntegerField("Points / s Holding Point")
+								{ value = settings.pointsPerSecondHoldingPoint };
+							pps.RegisterValueChangedCallback(evt =>
+								settings.pointsPerSecondHoldingPoint = (byte)Mathf.Clamp(evt.newValue, 0, 255));
+							matchSettingsPage.Add(pps);
+
+							var winDropdown = new EnumField("Win Condition", settings.winCondition);
+							winDropdown.RegisterValueChangedCallback(evt =>
+								settings.winCondition = (WinCondition)evt.newValue);
+							matchSettingsPage.Add(winDropdown);
+
+							var timer = new IntegerField("Timer Seconds") { value = settings.timerSeconds };
+							timer.RegisterValueChangedCallback(evt =>
+								settings.timerSeconds = Mathf.Max(0, evt.newValue));
+							matchSettingsPage.Add(timer);
+
+							var score = new IntegerField("Score Target") { value = settings.scoreTarget };
+							score.RegisterValueChangedCallback(evt =>
+								settings.scoreTarget =
+									(short)Mathf.Clamp(evt.newValue, short.MinValue, short.MaxValue));
+							matchSettingsPage.Add(score);
+
+							var startGame = new Button(() => { MatchReferee.Instance.StartMatchRpc(settings); })
+							{
+								text = "Start Game"
+							};
+							startGame.style.height = 24;
+							matchSettingsPage.Add(startGame);
+						}
+						matchPages.Add(matchSettingsPage);
+						
+						
+						matchRunningPage = new VisualElement();
+						{
+
+							var matchRunningLabel = new Label("Match Running")
+								{ style = { unityFontStyleAndWeight = FontStyle.Bold, marginTop = 10 } };
+							matchRunningPage.Add(matchRunningLabel);
+
+							var stopGame = new Button(() => { MatchReferee.Instance.EndMatchRpc(); })
+							{
+								text = "Stop Game",
+								style =
+								{
+									height = 24
+								}
+							};
+							matchRunningPage.Add(stopGame);
+						}
+						matchPages.Add(matchRunningPage);
+					}
+					connectedPage.Add(matchPages);
+				}
+				networkPages.Add(connectedPage);
+			}
 			rootVisualElement.Add(networkPages);
 
 
-			// host settings menu
-			startServerPage = new VisualElement();
-			networkPages.Add(startServerPage);
-			
-			startServerPage.Add(new Label("Host Settings") { style = { unityFontStyleAndWeight = FontStyle.Bold } });
-
-			var tagField = new FloatField("AprilTag size (cm)") { value = tagSizeCm };
-			tagField.RegisterValueChangedCallback(evt =>
-			{
-				tagSizeCm = Mathf.Max(0f, evt.newValue);
-				EditorPrefs.SetFloat(TagSizeSaveKey, tagSizeCm);
-			});
-			startServerPage.Add(tagField);
-
-			
-			var protocolPages = new PageParentElement();
-			
-			var lanPage = new VisualElement();
-			protocolPages.Add(lanPage);
-			
-			var ipField = new TextField("IP") { value = ipAddress };
-			ipField.RegisterValueChangedCallback(evt =>
-			{
-				ipAddress = evt.newValue;
-				EditorPrefs.SetString(IpSaveKey, ipAddress);
-			});
-			lanPage.Add(ipField);
-			
-			var relayPage = new VisualElement();
-			protocolPages.Add(relayPage);
-			
-			var roomNameField = new TextField("Room Name") { value = roomName };
-			roomNameField.RegisterValueChangedCallback(evt =>
-			{
-				roomName = Regex.Replace(evt.newValue, @"[^a-zA-Z0-9]", "");
-				roomNameField.SetValueWithoutNotify(roomName);
-				EditorPrefs.SetString(RoomNameSaveKey, roomName);
-			});
-			relayPage.Add(roomNameField);
-			
-			protocolPages.SetActiveElement(useRelay ? relayPage : lanPage);
-			
-			
-			var useRelayField = new Toggle("Use Relay") { value = useRelay };
-			useRelayField.RegisterValueChangedCallback(evt =>
-			{
-				useRelay = evt.newValue;
-				EditorPrefs.SetBool(UseRelaySaveKey, useRelay);
-
-				protocolPages.SetActiveElement(useRelay ? relayPage : lanPage);
-			});
-			startServerPage.Add(useRelayField);
-			
-			startServerPage.Add(protocolPages);
-			
-
-			var hostButton = new Button(() =>
-			{
-				if (EditorApplication.isPlaying)
-					StartHost();
-				else
-					EditorApplication.EnterPlaymode();
-			})
-			{
-				text = "Host",
-				style =
-				{
-					height = 32
-				}
-			};
-			startServerPage.Add(hostButton);
-			
-			startServerPage.Add(new Label("If using a Windows hotspot, remember to give Unity an exception in Windows Firewall.")
-			{
-				style = {
-					whiteSpace = WhiteSpace.Normal,
-				}
-			});
-
-			// connecting
-			connectingPage = new VisualElement();
-			networkPages.Add(connectingPage);
-			
-			connectingPage.Add(new Label("Connecting...") { style = { unityFontStyleAndWeight = FontStyle.Bold } });
-			//
-			// var cancelButton = new Button(NetcodeManagement.Disconnect)
-			// {
-			// 	text = "Cancel",
-			// 	style =
-			// 	{
-			// 		height = 24
-			// 	}
-			// };
-			// connectingPage.Add(cancelButton);
-
-			// hosting menu
-			connectedPage = new VisualElement();
-			networkPages.Add(connectedPage);
-			
-			connectedPage.Add(new Label("Hosting") { style = { unityFontStyleAndWeight = FontStyle.Bold } });
-
-			var stopButton = new Button(NetcodeManagement.Disconnect)
-			{
-				text = "Stop Hosting",
-				style =
-				{
-					height = 24
-				}
-			};
-			connectedPage.Add(stopButton);
-			
-			// match pages
-			matchPages = new PageParentElement();
-			connectedPage.Add(matchPages);
-
-
-			// match settings menu
-			matchSettingsPage = new VisualElement();
-			matchPages.Add(matchSettingsPage);
-
-			var matchSettingsLabel = new Label("Match Settings") { style = { unityFontStyleAndWeight = FontStyle.Bold, marginTop = 10 } };
-			matchSettingsPage.Add(matchSettingsLabel);
-
-			var respawnToggle = new Toggle("Respawn In Bases") { value = settings.respawnInBases };
-			respawnToggle.RegisterValueChangedCallback(evt => settings.respawnInBases = evt.newValue);
-			matchSettingsPage.Add(respawnToggle);
-
-			var respawnTime = new FloatField("Respawn Seconds") { value = settings.respawnSeconds };
-			respawnTime.RegisterValueChangedCallback(evt => settings.respawnSeconds = Mathf.Max(0f, evt.newValue));
-			matchSettingsPage.Add(respawnTime);
-
-			var regen = new FloatField("Health Regen / s") { value = settings.healthRegenPerSecond };
-			regen.RegisterValueChangedCallback(evt => settings.healthRegenPerSecond = Mathf.Max(0f, evt.newValue));
-			matchSettingsPage.Add(regen);
-
-			var ppk = new IntegerField("Points Per Kill") { value = settings.pointsPerKill };
-			ppk.RegisterValueChangedCallback(evt => settings.pointsPerKill = (byte)Mathf.Clamp(evt.newValue, 0, 255));
-			matchSettingsPage.Add(ppk);
-
-			var pps = new IntegerField("Points / s Holding Point") { value = settings.pointsPerSecondHoldingPoint };
-			pps.RegisterValueChangedCallback(evt => settings.pointsPerSecondHoldingPoint = (byte)Mathf.Clamp(evt.newValue, 0, 255));
-			matchSettingsPage.Add(pps);
-
-			var winDropdown = new EnumField("Win Condition", settings.winCondition);
-			winDropdown.RegisterValueChangedCallback(evt => settings.winCondition = (WinCondition)evt.newValue);
-			matchSettingsPage.Add(winDropdown);
-
-			var timer = new IntegerField("Timer Seconds") { value = settings.timerSeconds };
-			timer.RegisterValueChangedCallback(evt => settings.timerSeconds = Mathf.Max(0, evt.newValue));
-			matchSettingsPage.Add(timer);
-
-			var score = new IntegerField("Score Target") { value = settings.scoreTarget };
-			score.RegisterValueChangedCallback(evt => settings.scoreTarget = (short)Mathf.Clamp(evt.newValue, short.MinValue, short.MaxValue));
-			matchSettingsPage.Add(score);
-
-			var startGame = new Button(() =>
-			{
-				MatchReferee.Instance.StartMatchRpc(settings);
-			})
-			{
-				text = "Start Game"
-			};
-			startGame.style.height = 24;
-			matchSettingsPage.Add(startGame);
-
-
-			// match running menu
-			matchRunningPage = new VisualElement();
-			matchPages.Add(matchRunningPage);
-			
-			var matchRunningLabel = new Label("Match Running") { style = { unityFontStyleAndWeight = FontStyle.Bold, marginTop = 10 } };
-			matchRunningPage.Add(matchRunningLabel);
-
-			var stopGame = new Button(() =>
-			{
-				MatchReferee.Instance.EndMatchRpc();
-			})
-			{
-				text = "Stop Game",
-				style =
-				{
-					height = 24
-				}
-			};
-			matchRunningPage.Add(stopGame);
-			
-			
 			UpdateHostingPage(NetcodeManagement.State);
-			
+
 			var matchReferee = MatchReferee.Instance;
 			var matchState = matchReferee == null ? MatchState.NotPlaying : matchReferee.State;
 			UpdateMatchPage(matchState);
