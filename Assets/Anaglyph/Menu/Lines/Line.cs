1b36ddd77fade9ddff2b860606fd453f0270e251
Julian Triveri <triveri@outlook.com>
2025-11-15 15:50:26 +0100

    misc

--- Assets/Anaglyph/Menu/Lines/Line.cs
+++ Assets/Anaglyph/Menu/Lines/Line.cs
@@ -10,65 +10,70 @@ namespace Anaglyph.Menu
 		public float thickness;
 
 		public List<Vector2> points;
-		
-		public static Vector2 FromAngle(float a) => new (Mathf.Cos(a), Mathf.Sin(a));
+
+		public float firstAngle = 0;
+		public bool overrideFirstAngle = false;
+
+		public static Vector2 FromAngle(float a)
+		{
+			return new Vector2(Mathf.Cos(a), Mathf.Sin(a));
+		}
 
 		protected override void OnPopulateMesh(VertexHelper vh)
 		{
 			vh.Clear();
 
 			if (points.Count < 2) return;
-			
-			UIVertex vertex = UIVertex.simpleVert;
+
+			var vertex = UIVertex.simpleVert;
 			vertex.color = color;
+
+			var end = points.Count - 1;
 
 			for (var i = 0; i < points.Count; i++)
 			{
-				bool onFirst = i == 0;
-				bool onLast = i == points.Count - 1;
-				
-				var v = points[i];
+				var p = points[i];
 
 				var quarter = Mathf.PI / 2;
-				Vector2 na = Vector2.zero;
-				Vector2 nb = Vector2.zero;
-				Vector2 vec = Vector2.zero;
+				var nNext = Vector2.zero;
+				var nPrev = Vector2.zero;
+				var n = Vector2.zero;
 
-				if (!onLast)
+				if (i < end)
 				{
-					var p = points[i + 1];
-					var a = Mathf.Atan2(p.y - v.y, p.x - v.x) + quarter;
-					na = FromAngle(a);
-					vec += na;
+					var pNext = points[i + 1];
+					var a = Mathf.Atan2(pNext.y - p.y, pNext.x - p.x) + quarter;
+					nNext = FromAngle(a);
+					n += nNext;
 				}
 
-				if (!onFirst)
+				if (i > 0)
 				{
-					var p = points[i - 1];
-					var a = Mathf.Atan2(p.y - v.y, p.x - v.x) - quarter;
-					nb = FromAngle(a);
-					vec += nb;
-				}
-				
-				vec.Normalize();
-
-				if (!onFirst && !onLast)
-				{
-					var l = Mathf.Sqrt(2) / Mathf.Sqrt(1 + Vector2.Dot(na, nb));
-					vec *= l;
+					var pPrev = points[i - 1];
+					var a = Mathf.Atan2(pPrev.y - p.y, pPrev.x - p.x) - quarter;
+					nPrev = FromAngle(a);
+					n += nPrev;
 				}
 
-				float halfThickness = thickness / 2f;
+				n.Normalize();
 
-				vertex.position = v + vec * halfThickness;
+				if (i > 0 && i < end)
+				{
+					var l = Mathf.Sqrt(2) / Mathf.Sqrt(1 + Vector2.Dot(nNext, nPrev));
+					n *= l;
+				}
+
+				var halfThickness = thickness / 2f;
+
+				vertex.position = p + n * halfThickness;
 				vh.AddVert(vertex);
 
-				vertex.position = v - vec * halfThickness;
+				vertex.position = p - n * halfThickness;
 				vh.AddVert(vertex);
 
-				if (!onLast)
+				if (i < end)
 				{
-					int triIndex = i * 2;
+					var triIndex = i * 2;
 					vh.AddTriangle(triIndex + 2, triIndex + 1, triIndex + 0);
 					vh.AddTriangle(triIndex + 2, triIndex + 3, triIndex + 1);
 				}
--- Assets/Anaglyph/XRTemplate/Depth/v2/EnvMapper2.cs
+++ Assets/Anaglyph/XRTemplate/Depth/v2/EnvMapper2.cs
@@ -1,35 +1,62 @@
+using System.Threading.Tasks;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 namespace Anaglyph.DepthKit
 {
-    public class EnvMapper2 : MonoBehaviour
-    {
-	    public EnvMapper2 Instance { get; private set; }
-	    
-	    [SerializeField] private ComputeShader comp;
-	    
-	    private ComputeKernel Integrate;
-	    private ComputeBuffer blocks;
-	    
-	    private const int NumBlocks = 1024 * 1024;
+	public class EnvMapper2 : MonoBehaviour
+	{
+		public EnvMapper2 Instance { get; private set; }
 
-	    [GenerateHLSL(PackingRules.Exact, false)]
-	    public struct Block
-	    {
-		    public int3 pos_ws;   // 24
-		    public uint offset;   //  8
-		    public uint ptr;      //  8
-		    public uint3 padding; // 24
-		    public const int Size =  32;
-	    }
+		[SerializeField] private ComputeShader comp;
 
-	    private void Awake()
-	    {
-		    Instance = this;
+		private ComputeKernel Integrate;
+		private ComputeBuffer blocks;
 
-		    blocks = new ComputeBuffer(NumBlocks, Block.Size);
-	    }
-    }
+		[GenerateHLSL(PackingRules.Exact, false)]
+		public struct Block
+		{
+			public int3 pos_ws; // 24
+			public uint offset; //  8
+			public uint ptr; //  8
+			public uint3 padding; // 24
+			public const int Size = 32;
+		}
+
+		private void Awake()
+		{
+			Instance = this;
+
+			blocks = new ComputeBuffer(512, Block.Size);
+
+			Integrate.Set("blocks", blocks);
+		}
+
+		private async Task ExpandBuffer()
+		{
+			var tcs = new TaskCompletionSource<AsyncGPUReadbackRequest>();
+
+			AsyncGPUReadback.Request(blocks, (req) =>
+			{
+				if (req.hasError)
+					tcs.SetException(new System.Exception("GPU readback failed"));
+				else
+					tcs.SetResult(req);
+			});
+
+			var result = await tcs.Task;
+
+			if (result.hasError)
+				return;
+
+			var data = result.GetData<Block>();
+			var oldCount = blocks.count;
+			blocks.Dispose();
+			blocks = new ComputeBuffer(oldCount * 2, Block.Size);
+			blocks.SetData(data);
+
+			Integrate.Set("blocks", blocks);
+		}
+	}
 }
