7f734f83bc0dd72a5de22388bdb8ae02d4204fe2
cellomonster <treivier@gmail.com>
2025-12-01 15:03:38 -0500

    working burst multi-threaded surfacenet mesher

--- Assets/Anaglyph/XRTemplate/Depth/Meshing/Mesher.cs
+++ Assets/Anaglyph/XRTemplate/Depth/Meshing/Mesher.cs
@@ -1,10 +1,282 @@
-using System;
-using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 namespace Anaglyph.DepthKit
 {
-	
+	public class Mesher
+	{
+
+		private static readonly byte[] EdgeCornerA = {
+			0, 1, 2, 3,
+			4, 5, 6, 7,
+			0, 1, 2, 3
+		};
+
+		private static readonly byte[] EdgeCornerB = {
+			1, 2, 3, 0,
+			5, 6, 7, 4,
+			4, 5, 6, 7
+		};
+
+		private static readonly int3[] CornerCoords =
+		{
+			new(0, 0, 0),
+			new(1, 0, 0),
+			new(1, 0, 1),
+			new(0, 0, 1),
+
+			new(0, 1, 0),
+			new(1, 1, 0),
+			new(1, 1, 1),
+			new(0, 1, 1),
+		};
+
+		private static readonly int3 X = new(1, 0, 0);
+		private static readonly int3 Y = new(0, 1, 0);
+		private static readonly int3 Z = new(0, 0, 1);
+
+		public async Task CreateMesh(NativeArray<sbyte> volume, int3 volumeSize, float metersPerVoxel, Mesh mesh)
+		{
+			int vertCountEstimate = volume.Length / 10;
+			NativeList<float3> verts = new(vertCountEstimate, Allocator.TempJob);
+			NativeList<int3> vertCoords = new(vertCountEstimate, Allocator.TempJob);
+			
+			NativeArray<uint> vertexIndices = new(volume.Length, Allocator.TempJob);
+
+			int triCountEstimate = vertCountEstimate * 6;
+			NativeList<uint> tris = new(triCountEstimate, Allocator.TempJob);
+
+			VertexJob vertexMaker = new()
+			{
+				Volume = volume,
+				VolumeSize = volumeSize,
+				MetersPerVoxel = metersPerVoxel,
+				
+				VertexIndices = vertexIndices,
+				Verts = verts.AsParallelWriter(),
+				VertCoords = vertCoords.AsParallelWriter(),
+			};
+
+			JobHandle vertHandle = vertexMaker.ScheduleParallelByRef(volume.Length, 1, default);
+			while (!vertHandle.IsCompleted) await Awaitable.NextFrameAsync();
+			vertHandle.Complete();
+			
+			IndexJob triMaker = new()
+			{
+				VertCoords = vertCoords.AsArray(),
+				VertIndices = vertexIndices,
+
+				Volume = volume,
+				VolumeSize = volumeSize,
+
+				Tris = tris.AsParallelWriter(),
+			};
+			
+			JobHandle triHandle = triMaker.ScheduleParallelByRef(vertCoords.Length, 1, vertHandle);
+			while (!triHandle.IsCompleted) await Awaitable.NextFrameAsync();
+			triHandle.Complete();
+
+			ApplyToMesh(verts, tris, mesh);
+
+			vertexIndices.Dispose();
+			verts.Dispose();
+			vertCoords.Dispose();
+			tris.Dispose();
+		}
+
+		private void ApplyToMesh(NativeList<float3> verts, NativeList<uint> tris, Mesh mesh)
+		{
+			const MeshUpdateFlags flags = MeshUpdateFlags.DontNotifyMeshUsers | MeshUpdateFlags.DontRecalculateBounds |
+			                              MeshUpdateFlags.DontNotifyMeshUsers;
+
+			VertexAttributeDescriptor vad = new (VertexAttribute.Position);
+			mesh.SetVertexBufferParams(verts.Length, vad);
+			mesh.SetVertexBufferData(verts.AsArray(), 0, 0, verts.Length, 0, flags);
+			
+			mesh.SetIndexBufferParams(tris.Length, IndexFormat.UInt32);
+			mesh.SetIndexBufferData(tris.AsArray(), 0, 0, tris.Length, flags);
+			
+			mesh.subMeshCount = 1;
+			SubMeshDescriptor smd = new(0, tris.Length);
+			mesh.SetSubMesh(0, smd);
+
+			mesh.RecalculateTangents(flags);
+			mesh.RecalculateNormals(flags);
+
+			mesh.RecalculateBounds();
+		}
+		
+		[BurstCompile]
+		private struct VertexJob : IJobFor
+		{
+			[ReadOnly] public NativeArray<sbyte> Volume;
+			[ReadOnly] public int3 VolumeSize;
+			[ReadOnly] public float MetersPerVoxel;
+
+			[WriteOnly] public NativeArray<uint> VertexIndices;
+			[WriteOnly] public NativeList<float3>.ParallelWriter Verts;
+			[WriteOnly] public NativeList<int3>.ParallelWriter VertCoords;
+			
+			public unsafe void Execute(int threadIdx)
+			{
+				int3 coord = ThreadIndexToCoord(threadIdx);
+
+				if (coord.x == VolumeSize.x - 1 || coord.y == VolumeSize.y - 1 || coord.z == VolumeSize.z - 1)
+					return;
+
+				float* values = stackalloc float[8];
+				float3* positions = stackalloc  float3[8];
+
+				for (int i = 0; i < 8; i++)
+				{
+					int3 offs = coord + CornerCoords[i];
+					int dataIndex = CoordToDataIndex(offs);
+					values[i] = Volume[dataIndex];
+					positions[i] = CoordToPos(offs);
+				}
+
+				float3 pos = new();
+				byte numCrossings = 0;
+				
+				for (int i = 0; i < 12; i++)
+				{
+					byte a = EdgeCornerA[i];
+					byte b = EdgeCornerB[i];
+					
+					float fa = values[a] / 127f;
+					float fb = values[b] / 127f;
+
+					bool crosses = (fa < 0) != (fb < 0);
+					if (crosses)
+					{
+						numCrossings++;
+
+						float3 pa = positions[a];
+						float3 pb = positions[b];
+						
+						float t = fa / (fa - fb);
+						pos += pa + t * (pb - pa);
+					}
+				}
+
+				if (numCrossings == 0) return;
+
+				float inv = math.rcp(numCrossings);
+				pos *= inv;
+
+				UnsafeList<float3>* vertsUnsafe = Verts.ListData;
+				if (vertsUnsafe->m_length >= vertsUnsafe->Capacity) return;
+				var idx = Interlocked.Increment(ref vertsUnsafe->m_length) - 1;
+				if (vertsUnsafe->m_length >= vertsUnsafe->Capacity) return; // capacity exceeded
+				UnsafeUtility.WriteArrayElement(vertsUnsafe->Ptr, idx, pos);
+				
+				VertCoords.AddNoResize(coord);
+				VertexIndices[threadIdx] = (uint)idx;
+			}
+
+			private int3 ThreadIndexToCoord(int i)
+			{
+				int3 c;
+				int3 s = VolumeSize;
+				c.x = i % s.x;
+				c.y = (i / s.x) % s.y;
+				c.z = i / (s.x * s.y);
+				return c;
+			}
+			
+			private int CoordToDataIndex(int3 coord)
+			{
+				int3 c = coord;
+				int3 s = VolumeSize;
+				return c.x + c.y * s.x + c.z * s.x * s.y;
+			}
+			
+			private float3 CoordToPos(int3 c)
+			{
+				return (float3)c * MetersPerVoxel;
+			}
+		}
+		
+		[BurstCompile]
+		private struct IndexJob : IJobFor
+		{
+			[ReadOnly] public NativeArray<int3> VertCoords;
+			[ReadOnly] public NativeArray<uint> VertIndices;
+			
+			[ReadOnly] public NativeArray<sbyte> Volume;
+			[ReadOnly] public int3 VolumeSize;
+
+			[WriteOnly] public NativeList<uint>.ParallelWriter Tris;
+			
+			public void Execute(int threadIndex)
+			{
+				int3 coord = VertCoords[threadIndex];
+				
+				if(coord.x == 0 ||  coord.y == 0 || coord.z == 0)
+					return;
+
+				TrisForAxis(coord, X, Z, Y);
+				TrisForAxis(coord, Y, X, Z);
+				TrisForAxis(coord, Z, Y, X);
+			}
+			
+			private unsafe void TrisForAxis(int3 coord, int3 axis, int3 d1, int3 d2)
+			{
+				int ia = CoordToDataIndex(coord);
+				float fa = Volume[ia] / 127f;
+			
+				int3 ca = coord + axis;
+				int ib = CoordToDataIndex(ca);
+				float fb = Volume[ib] / 127f;
+
+				bool negA = (fa < 0);
+				bool negB = (fb < 0);
+
+				if (negA != negB)
+				{
+					uint a = VertIndices[ia];
+					uint b = VertIndices[CoordToDataIndex(coord - d1)];
+					uint c = VertIndices[CoordToDataIndex(coord - (d1 + d2))];
+					uint d = VertIndices[CoordToDataIndex(coord - d2)];
+
+					UnsafeList<uint>* trisUnsafe = Tris.ListData;
+					if (trisUnsafe->m_length >= trisUnsafe->Capacity) return;
+					var idx = Interlocked.Add(ref trisUnsafe->m_length, 6) - 6;
+					if (trisUnsafe->m_length >= trisUnsafe->Capacity) return; // capacity exceeded
+					
+					if (negA)
+					{
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 0, c);
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 1, b);
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 2, a);
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 3, d);
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 4, c);
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 5, a);
+					}
+					else
+					{
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 0, a);
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 1, c);
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 2, d);
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 3, a);
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 4, b);
+						UnsafeUtility.WriteArrayElement(trisUnsafe->Ptr, idx + 5, c);
+					}
+				}
+			}
+			
+			private int CoordToDataIndex(int3 c)
+			{
+				int3 s = VolumeSize;
+				return c.x + c.y * s.x + c.z * s.x * s.y;
+			}
+		}
+	}
 }
--- Assets/Anaglyph/XRTemplate/Depth/Meshing/Test.cs
+++ Assets/Anaglyph/XRTemplate/Depth/Meshing/Test.cs
@@ -1,10 +1,10 @@
-using System;
 using Anaglyph.XRTemplate;
+using Unity.Burst;
 using Unity.Collections;
+using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Serialization;
 
 namespace Anaglyph.DepthKit
 {
@@ -13,19 +13,14 @@ namespace Anaglyph.DepthKit
 		[SerializeField] private EnvironmentMapper mapper;
 		[SerializeField] private MeshFilter meshFilter;
 
-		[SerializeField] public int3 start;
-		[SerializeField] public int3 end;
-
 		private Mesh mesh;
 		
-		MesherPrototype mesher = new ();
+		private Mesher mesher = new();
 
 		private void Start()
 		{
-			mesher.metersPerVoxel = mapper.MetersPerVoxel;
-			mesher.voxelCount = new int3(mapper.vWidth, mapper.vHeight, mapper.vDepth);
-
 			mesh = new Mesh();
+			meshFilter.mesh = mesh;
 
 			IntegrateLoop();
 		}
@@ -34,23 +29,47 @@ namespace Anaglyph.DepthKit
 		{
 			while (enabled)
 			{
-				await Awaitable.WaitForSecondsAsync(1);
+				await Awaitable.WaitForSecondsAsync(0.1f);
 				
 				AsyncGPUReadbackRequest req = await AsyncGPUReadback.RequestAsync(mapper.Volume);
 
-				sbyte[] full = new sbyte[req.width * req.height * req.depth];
 				int sliceSize = req.width * req.height;
+				
+				NativeArray<sbyte> volume = new(sliceSize * req.depth, Allocator.TempJob);
 
 				for (int z = 0; z < req.layerCount; z++)
 				{
 					NativeArray<sbyte> slice = req.GetData<sbyte>(z);
+					int dstOffset = z * req.width * req.height;
 
-					slice.ToArray().CopyTo(full, z * sliceSize);
+					CopySliceJob copier = new()
+					{
+						Source = slice,
+						Destination = volume,
+						DestOffset = dstOffset
+					};
+					
+					copier.ScheduleParallelByRef(sliceSize, 16, default).Complete();
 				}
 
-				mesher.Data = full;
-				mesher.BuildMesh(start, end, mesh);
-				meshFilter.mesh = mesh;
+				await mesher.CreateMesh(volume, new int3(req.width, req.height, req.depth), mapper.MetersPerVoxel, mesh);
+				
+				volume.Dispose();
+			}
+		}
+		
+		[BurstCompile]
+		private struct CopySliceJob : IJobFor
+		{
+			[ReadOnly] public NativeArray<sbyte> Source;
+			public int DestOffset;
+			
+			[NativeDisableParallelForRestriction]
+			[WriteOnly] public NativeArray<sbyte> Destination;
+
+			public void Execute(int i)
+			{
+				Destination[DestOffset + i] = Source[i];
 			}
 		}
 
