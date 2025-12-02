1f4f0899043481e0abe1118d4159eef0049015d7
Julian Triveri <triveri@outlook.com>
2025-12-02 01:50:57 -0500

    a

--- Assets/Anaglyph/XRTemplate/Depth/Meshing/ChunkManager.cs
+++ Assets/Anaglyph/XRTemplate/Depth/Meshing/ChunkManager.cs
@@ -1,10 +1,7 @@
-using System;
 using System.Collections.Generic;
-using System.Drawing.Drawing2D;
 using Anaglyph.XRTemplate;
 using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.XR;
 
 namespace Anaglyph.DepthKit.Meshing
@@ -12,52 +9,54 @@ namespace Anaglyph.DepthKit.Meshing
 	public class ChunkManager : MonoBehaviour
 	{
 		private EnvironmentMapper mapper => EnvironmentMapper.Instance;
-		
+
 		[SerializeField] private float3 chunkSize = new(5, 5, 5);
 		[SerializeField] private GameObject chunkPrefab;
-		
+
 		[SerializeField] private float updateFrequency = 0.1f;
 
 		[SerializeField] private Vector3[] frustumMeshTriggerPoints;
-		
+
 		private Dictionary<int3, MeshChunk> chunks = new();
 		private List<int3> updateList = new();
 		private Camera mainCamera;
-		
+
 		public void OnEnable()
 		{
 			if (!XRSettings.enabled)
 				return;
-			
+
 			UpdateLoop();
+		}
+
+		private void FixedUpdate()
+		{
+			if (!mainCamera)
+			{
+				mainCamera = Camera.main;
+				return;
+			}
+
+			foreach (Vector3 local in frustumMeshTriggerPoints)
+			{
+				Transform camTrans = mainCamera.transform;
+				float3 global = camTrans.TransformPoint(local);
+				int3 coord = PosToChunkCoord(global);
+
+				if (!updateList.Contains(coord))
+					updateList.Add(coord);
+			}
 		}
 
 		private async void UpdateLoop()
 		{
 			while (enabled)
 			{
-				await Awaitable.WaitForSecondsAsync(updateFrequency);
-
-				if (!mainCamera)
+				if (updateList.Count > 0)
 				{
-					mainCamera = Camera.main;
-					continue;
-				}
-				
-				Transform camTrans = mainCamera.transform;
+					int3 coord = updateList[0];
+					updateList.Remove(0);
 
-				updateList.Clear();
-				foreach (Vector3 local in frustumMeshTriggerPoints)
-				{
-					float3 global = camTrans.TransformPoint(local);
-					int3 coord = PosToChunkCoord(global);
-					
-					if (!updateList.Contains(coord))
-						updateList.Add(coord);
-				}
-
-				foreach (int3 coord in updateList)
-				{
 					bool foundChunk = chunks.TryGetValue(coord, out MeshChunk chunk);
 
 					if (!foundChunk)
@@ -65,17 +64,19 @@ namespace Anaglyph.DepthKit.Meshing
 
 					await chunk.Mesh();
 				}
+
+				await Awaitable.WaitForSecondsAsync(updateFrequency);
 			}
 		}
-		
+
 #if UNITY_EDITOR
 		private void OnDrawGizmos()
 		{
 			Transform t = transform;
 			if (mainCamera) t = mainCamera.transform;
-			
+
 			Gizmos.color = new Color(1f, 0.5f, 0f, 1f);
-			
+
 			foreach (Vector3 local in frustumMeshTriggerPoints)
 			{
 				float3 global = t.TransformPoint(local);
@@ -91,7 +92,7 @@ namespace Anaglyph.DepthKit.Meshing
 
 			float connectionPadding = 3 * mapper.MetersPerVoxel;
 			chunk.extents = chunkSize + connectionPadding;
-			
+
 			chunk.transform.position = ChunkCoordToPos(chunkCoord);
 
 			chunks.Add(chunkCoord, chunk);
