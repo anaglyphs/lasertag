4954f9126129565146794233842d69fb44951254
Julian Triveri <triveri@outlook.com>
2026-01-11 19:22:47 -0500

    wip switch to unity sim

--- Assets/Anaglyph/LaserTag/CameraRigSpawner.cs
+++ Assets/Anaglyph/LaserTag/CameraRigSpawner.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
 
 namespace Anaglyph.Lasertag
 {
-    public class CameraRigSpawner : MonoBehaviour
-    {
+	public class CameraRigSpawner : MonoBehaviour
+	{
 		[SerializeField] private GameObject xrRig;
 		[SerializeField] private GameObject desktopRig;
 
 		private void Awake()
 		{
-			Instantiate(XRSettings.enabled ? xrRig : desktopRig);
+			bool usingXR = XRSettings.enabled || XRInteractionSimulator.instance.enabled;
+			Instantiate(usingXR ? xrRig : desktopRig);
 		}
 	}
 }
