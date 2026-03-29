c06810c59f38fcf356985f0886e66d121d071f3c
Prageeth Monasha <spmonasha@gmail.com>
2026-03-29 03:56:24 +0200

    ✅ TSDF volume integration is working on M3 Max
    What's running on the M3 Max now:
    
    Depth dilation (8-step ping-pong)
    Normal generation from depth
    TSDF volume integration

--- Assets/Monasha/EdgeServer/EdgeServerClient.cs
+++ Assets/Monasha/EdgeServer/EdgeServerClient.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using System.Net.Sockets;
 using Anaglyph;
+using Anaglyph.XRTemplate;
 using Anaglyph.XRTemplate.DepthKit;
 using Unity.Collections;
 using UnityEngine.Rendering;
@@ -40,6 +41,11 @@ namespace Monasha.EdgeServer
 
 			// subscribe to depth updates
 			DepthKitDriver.Instance.Updated += OnDepthUpdated;
+
+			var mapper = EnvironmentMapper.Instance;
+			if (mapper != null)
+				Debug.Log(
+					$"Volume: {mapper.VoxelCount}, voxelSize: {mapper.VoxelSize}, voxelDist: {mapper.VoxelDistance}");
 		}
 
 		private void Update()
@@ -89,7 +95,7 @@ namespace Monasha.EdgeServer
 				var data = request.GetData<byte>();
 				var bytes = data.ToArray();
 
-				var matrices = SerializeMatrices();
+				var matrices = SerializeFrameData();
 				var payload = new byte[matrices.Length + bytes.Length];
 				Buffer.BlockCopy(matrices, 0, payload, 0, matrices.Length);
 				Buffer.BlockCopy(bytes, 0, payload, matrices.Length, bytes.Length);
@@ -109,31 +115,53 @@ namespace Monasha.EdgeServer
 		}
 
 
-		private byte[] SerializeMatrices()
+		private byte[] SerializeFrameData()
 		{
-			var dkd = DepthKitDriver.Instance;
+			DepthKitDriver dkd = DepthKitDriver.Instance;
+			EnvironmentMapper mapper = EnvironmentMapper.Instance;
 
-			// 4 matrix arrays x 2 eyes x 64 bytes each = 512 bytes
-			var data = new byte[512];
-			var offset = 0;
+			// 512 bytes matrices + 28 bytes volume config = 540 bytes
+			byte[] data = new byte[540];
+			int offset = 0;
 
-			for (var eye = 0; eye < 2; eye++) WriteMatrix(dkd.View[eye]);
-			for (var eye = 0; eye < 2; eye++) WriteMatrix(dkd.Proj[eye]);
-			for (var eye = 0; eye < 2; eye++) WriteMatrix(dkd.ViewInv[eye]);
-			for (var eye = 0; eye < 2; eye++) WriteMatrix(dkd.ProjInv[eye]);
+			void WriteFloat(float f)
+			{
+				byte[] b = BitConverter.GetBytes(f);
+				Buffer.BlockCopy(b, 0, data, offset, 4);
+				offset += 4;
+			}
 
-			return data;
+			void WriteInt(int i)
+			{
+				byte[] b = BitConverter.GetBytes(i);
+				Buffer.BlockCopy(b, 0, data, offset, 4);
+				offset += 4;
+			}
 
 			void WriteMatrix(Matrix4x4 m)
 			{
-				for (var i = 0; i < 16; i++)
-				{
-					var floatBytes = BitConverter.GetBytes(m[i]);
-					Buffer.BlockCopy(floatBytes, 0, data, offset, 4);
-					offset += 4;
-				}
+				for (int i = 0; i < 16; i++)
+					WriteFloat(m[i]);
 			}
+
+			// Matrices (512 bytes)
+			for (int eye = 0; eye < 2; eye++) WriteMatrix(dkd.View[eye]);
+			for (int eye = 0; eye < 2; eye++) WriteMatrix(dkd.Proj[eye]);
+			for (int eye = 0; eye < 2; eye++) WriteMatrix(dkd.ViewInv[eye]);
+			for (int eye = 0; eye < 2; eye++) WriteMatrix(dkd.ProjInv[eye]);
+
+			// Volume config (28 bytes)
+			WriteInt(mapper.VoxelCount.x);
+			WriteInt(mapper.VoxelCount.y);
+			WriteInt(mapper.VoxelCount.z);
+			WriteFloat(mapper.VoxelSize);
+			WriteFloat(mapper.VoxelDistance);
+			WriteFloat(mapper.MaxUpdateDist);
+			WriteInt(mapper.PlayerHeads.Count);
+
+			return data;
 		}
+
 
 		private void OnDestroy()
 		{
